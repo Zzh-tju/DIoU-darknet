87a33a6403dc065a483dc885e097bb02de10c680
Zzh-tju <57825730+Zzh-tju@users.noreply.github.com>
2019-12-19 17:28:30 +0800

    Add files via upload

---
+++ eval/compute_mAP.py
@@ -0,0 +1,42 @@
+from voc_eval import voc_eval
+for i in range(9):
+    thresh = (i*5+50)/100
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'aeroplane', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'bicycle', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'bird', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'boat', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'bottle', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'bus', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'car', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'cat', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'chair', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'cow', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'diningtable', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'dog', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'horse', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'motorbike', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'person', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'pottedplant', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'sheep', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'sofa', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'train', '.', thresh))
+
+    print(voc_eval('/home/ubuntu/zzh/g-darknet-master/results/{}.txt', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/Annotations/{}.xml', '/home/ubuntu/zzh/g-darknet-master/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'tvmonitor', '.', thresh))
---
+++ eval/map.py
@@ -0,0 +1,17 @@
+import os
+path = "/home/yourpath/DIoU-Darknet/"     #Put all the mAP files (AP50, AP55,..., AP95) in this folder
+mAPlist = []
+for txt in os.listdir(path):
+    content = open(path+ '/' + txt, 'r')
+    line = content.readline()
+    sum_AP = 0.0
+    while line:
+        sum_AP += float(line.split()[1])
+        line = content.readline()
+    mAP = sum_AP / 20
+    mAPlist.append(mAP)
+    content.close()
+    content = open(path+ '/' + txt, 'a')
+    content.write('\n' + '%f'%mAP)
+    content.close()
+print(sum(mAPlist)/len(mAPlist))
